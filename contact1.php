1b0afd028f32db099f0c028acf8e27b27f4fa902
Dinithikw <Dinithikw@users.noreply.github.com>
2021-01-14 15:00:12 +0530

    contact.php file created

---
+++ contact1.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	  <title>Multidisciplinary Research and Development Centre</title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	  
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	  <link href="https://fonts.googleapis.com/css2?family=Yantramanav:wght@400;900&display=swap" rel="stylesheet">
+	  <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Gothic&display=swap" rel="stylesheet">
+	  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600;700&display=swap" rel="stylesheet">
+	  <link rel="preconnect" href="https://fonts.gstatic.com">
+	  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@600;700;800&display=swap" rel="stylesheet">
+	  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
+	  <link rel="stylesheet" href="css/style.css">
+	  <link rel="stylesheet" href="css/contact.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><b>Data Science Research Centre</b></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item" style="margin-right:25px;">
+        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#research-row" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Research
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="research.html#climate_change">Climatic Changes</a>
+          <a class="dropdown-item" href="research.html#food_security">Food security and modern farming</a>
+          <a class="dropdown-item" href="research.html#pharmaceuticals">Pharmaceuticals</a>
+		  <a class="dropdown-item" href="research.html#health">Health related predictive analysis</a>
+        </div>
+      </li>
+	  <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          About Us
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="about_us.html">Research Unit</a>
+          <a class="dropdown-item" href="members.html">Members</a>
+        </div>
+      </li>
+	  <li class="nav-item active">
+        <a class="nav-link" href="#">Contact<span class="sr-only"></span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+	<section id="research-head" class="margin">
+		<div class="row">
+			<div class="col-md-12 research-head">
+				<center>Contact Us</center>
+			</div>
+		</div>
+	</section>
+	
+	<div class="row">
+		<!--<div class=" col-md-12 content">-->
+			<div class="col-md-5 form">
+				<div class="quote">
+					Are you Innovative?<br>
+					Interested in Research?
+				</div>
+				<form action="mail.php" method="POST">
+				<div class="let">Let us know...</div>
+				<input type="text" name="name" placeholder="Name"><br>
+				<input type="text" name="email" placeholder="E-mail"><br>
+				<input type="text" name="subject" placeholder="Subject"><br>
+				<textarea name="message" placeholder="Message"></textarea>
+				<button type="submit" name="submit">Submit</button>
+				</form>
+			</div>
+			<div class="col-md-1 column">
+			</div>
+			<div class="col-md-5 details">
+				<table class="contact_details">
+					<tr>
+						<td><img src="img/logo_color.bmp" height="83px" width="64"></td>
+						<td style="font-size:79px;font-family:'Raleway', sans-serif;">DSRC</td>
+					</tr>
+					<tr>
+						<td><div class="icon-border"><img src="icon/7.png" class="icons" height="38px" width="38px;"></div></td>
+						<td>Data Science Research Centre<br>Faculty of Applied Sciences<br>Rajarata University of Sri Lanka<br>Mihintale</td>
+					</tr>
+					<tr>
+						<td><div class="icon-border"><img src="icon/6.png" class="icons" height="38px" width="38px"></div></td>
+						<td>025_2266125</td>
+					</tr>
+					<tr>
+						<td><div class="icon-border"><img src="icon/1.png" class="icons" height="38px" width="38px"></div></td>
+						<td>dean@aps.rjt.ac.lk</td>
+					</tr>
+					<tr>
+						<td><div class="icon-border"><img src="icon/3.png" class="icons" height="38px" width="38px"></div></td>
+						<td>+94-25-2266120</td>
+					</tr>
+				</table>
+			</div>
+		<!--</div>-->
+	</div>
+	<br>
+	
+	<footer>
+		
+		<div class="row">
+			
+			<div class="col-md-1 pt1 deskContent">
+				<img src="img/logo.png" width="150" height="200"><br>
+			</div>
+			<div class="col-md-12 phoneContent pt1-mobi">
+				<img src="img/logo.png" width="150" height="200"><br>
+			</div>
+	
+			<div class="col-md-4" style="font-size:20px; padding-left:0px; padding-top:100px; padding-right:0px;font-family: 'Poppins', sans-serif;">
+				<h4>Data Science Research Centre</h4>
+						Faculty of Applied Sciences
+				<br>Rajarata University of Sri Lanka
+			</div>
+			  
+			<div class="border-left d-sm-none d-md-block deskContent" style="width: 0px;"></div>
+
+			<div class="col-md-4" style="margin-left: -1px; padding-top: 20px;">
+				<hr class="d-sm-block d-md-none">
+				
+				<h5>Contact Us</h5>
+				<br>
+				<table>
+					<tr style="line-height: 1em;">
+						<td><i class="fa"><img src="icon/7.png" style="height: 30px; width: 30px;"></i></td>
+						<td>Data Science Research Center, Faculty of Applied Sciences,<br>Rajarata University<br>Sri Lanka</td>
+					</tr>
+					<tr style="line-height: 1em;">
+						<td><i class="fa"><img src="icon/6.png" style="height: 30px; width: 30px;"></i></td>
+						<td>+94-25 2266125</td>
+					</tr>
+					<tr style="line-height: 1em;">
+						<td><i class="fa"><img src="icon/4.png" style="height: 30px; width: 30px;"></i></td>
+						<td><a href="#">www.datascience.rjt.ac.lk</a></td>
+					</tr>
+					<tr style="line-height: 1em;">
+						<td><i class="fa"><img src="icon/1.png" style="height: 30px; width: 30px;"></i></td>
+						<td><a href="#">dean@aps.rjt.ac.lk</a></td>
+					<tr>
+					<tr style="line-height: 1em;">
+						<td><i class="fa"><img src="icon/3.png" style="height: 30px; width: 30px;"></i></td>
+						<td>+94-25-2266120</td>
+					</tr>
+				</table>
+
+			</div>
+			  
+			<div class="border-left d-sm-none d-md-block deskContent" style="width: 0px;"></div>
+			<div class="col-md-2" style="margin-left: -1px; padding-top: 20px;">
+					<hr class="d-sm-block d-md-none">
+					<div class="row">
+						<div class="col-md-12 research">
+							<h5>Research Areas</h5>
+							<br>
+							<a href="research.html" style="font-size: 12px; line-height: 2em;">Food security and modern farming</a><br>
+							<a href="research.html" style="font-size: 12px; line-height: 2em;">Health related predictive analysis</a><br>
+							<a href="research.html" style="font-size: 12px; line-height: 2em;">Pharmaceuticals</a><br>
+							<a href="research.html" style="font-size: 12px; line-height: 2em;">Climate Change</a><br>
+							<hr>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 connect">
+							<h5>Find Us:</h5>
+					
+							<table>
+								<tr>
+									<td><a href="#"><img src="icon/2.png" style="height: 50px; width: 50px;"></a></td>
+									<td><a href="#"><img src="icon/5.png" style="height: 50px; width: 50px;"></a></td>
+								</tr>
+							</table>
+						</div>
+					</div>
+			</div>
+
+		</div>
+	
+	</footer>
+
+	<div class="container-fluid col-sm-12">
+		<div class="copyright">
+			<p> © 2020 Copyright Data Science Research Center, RUSL.</p>
+		</div>
+
+	</div>
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+</body>
+</html>
